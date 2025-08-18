ad7a5a37a13f2bb9be026510fce41c59e764bd2d
gyf <2528357407@qq.com>
2025-08-19 00:47:26 +0800

    心灵树洞做倾听者聊天初步实现消息发送

--- backend/app/models/feedback_image.py
+++ backend/app/models/feedback_image.py
@@ -1,15 +1,3 @@
-# from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-# from sqlalchemy.sql import func
-# from app.database.session import Base
-
-# class FeedbackImage(Base):
-#     __tablename__ = "feedback_images"
-    
-#     image_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-#     feedback_id = Column(Integer, ForeignKey("user_feedbacks.feedback_id"), nullable=False)
-#     image_url = Column(String(500), nullable=False)
-#     created_at = Column(DateTime, server_default=func.now())
-
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
